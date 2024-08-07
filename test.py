51d379af8be3b3a24221f5c3cd9157059b16112f
Carlos <carlosprovisorio@icloud.com>
2024-08-07 15:21:24 -0400

    Sending Status code, Validating Requests

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(f"{BASE}video/1", {"likes": 10})
+# response = requests.put(
+#     f"{BASE}video/1", {"likes": 10, "name": "Carlos", "views": 100000}
+# )
+# print(response.json())
+# input()
+response = requests.get(f"{BASE}video/6")
 print(response.json())
