ea21a1d6f150e18124937f878a218fe897575ca4
Carlos <carlosprovisorio@icloud.com>
2024-08-07 21:35:44 -0400

    Serializing Objects, Adding entries to the database and handling update request

--- rest_api_you_tube.py
+++ rest_api_you_tube.py
@@ -19,9 +19,16 @@ class VideoModel(db.Model):
 
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument(
+    "name", type=str, help="Name of the video is required", required=True
+)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes of the video")
 
 resource_fields = {
     "id": fields.String,
@@ -50,6 +57,23 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+
+        db.session.commit()
+        return result
 
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
--- test.py
+++ test.py
@@ -2,17 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [
-    {"likes": 78, "name": "How to make REST API", "views": 20000},
-    {"likes": 100, "name": "System Design Interview", "views": 10000},
-    {"likes": 200, "name": "Python for everybody", "views": 30000},
-]
-
-for i in range(len(data)):
-    response = requests.put(f"{BASE}video/{str(i)}", data[i])
-    print(response.json())
-
-
-input()
-response = requests.get(f"{BASE}video/2")
+response = requests.patch(f"{BASE}video/2", {"views": 99, "likes": 101})
 print(response.json())
